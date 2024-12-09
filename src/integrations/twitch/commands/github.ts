ecff90a0221b75491ae193ca19418ec757853084
Ashlay Steur <ashlaysteur@intal.nl>
2024-12-09 17:32:08 +0100

    Updated github link to link to correct github profile

--- src/integrations/twitch/commands/github.ts
+++ src/integrations/twitch/commands/github.ts
@@ -1,26 +1,26 @@
-import EventBus from "../../../eventBus";
-import { ShouldThrottle } from "../shouldThrottle";
+import EventBus from '../../../eventBus';
+import { ShouldThrottle } from '../shouldThrottle';
 import { OnCommandEvent } from '../../../types/onCommandEvent';
-import { BotEvents } from "../../../botEvents";
+import { BotEvents } from '../../../botEvents';
 
 /**
  * Sends a message to chat with a link to Michael's GitHub profile
  * @param onCommandEvent
  */
 export const github = (onCommandEvent: OnCommandEvent): void => {
-  const cooldownSeconds = 300;
+	const cooldownSeconds = 300;
 
-  // The broadcaster is allowed to bypass throttling. Otherwise,
-  // only proceed if the command hasn't been used within the cooldown.
-  if (
-    !onCommandEvent.flags.broadcaster &&
-    ShouldThrottle(onCommandEvent.extra.sinceLastCommand, cooldownSeconds, true)
-  ) {
-    return;
-  }
+	// The broadcaster is allowed to bypass throttling. Otherwise,
+	// only proceed if the command hasn't been used within the cooldown.
+	if (
+		!onCommandEvent.flags.broadcaster &&
+		ShouldThrottle(onCommandEvent.extra.sinceLastCommand, cooldownSeconds, true)
+	) {
+		return;
+	}
 
-  const message = `All of our code can be found at https://github.com/build-with-me`;
+	const message = `All of our code can be found at https://github.com/michaeljolley`;
 
-  // Send the message to Twitch chat
-  EventBus.eventEmitter.emit(BotEvents.OnSay, { message });
-}
+	// Send the message to Twitch chat
+	EventBus.eventEmitter.emit(BotEvents.OnSay, { message });
+};
