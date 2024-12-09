844fc5e0d28b61b6f39d596924275f9561ace320
Michael Jolley <mike@sparcapp.io>
2024-12-09 15:22:50 -0600

    yo yo yo

--- src/web/js/chat.js
+++ src/web/js/chat.js
@@ -1,4 +1,4 @@
-const { computed, createApp, onMounted, onUnmounted, ref } = Vue
+const { computed, createApp, onMounted, onUnmounted, ref } = Vue;
 
 const isParty = ref(false);
 
@@ -9,37 +9,39 @@ const chat = createApp({
 		const shouldClear = ref(false);
 
 		const addMessage = (onChatMessageEvent) => {
-      messages.value = [onChatMessageEvent, ...messages.value];
-    }
-    const clearMessages = () => {
-      shouldClear.value = true;
-      const clearMSGTimeout = setTimeout(() => {
-        messages.value = [];
-        shouldClear.value = false;
-        clearTimeout(clearMSGTimeout);
-      }, 1200);
-    }
-    const removeMessages = (count) => {
-      messages.value = messages.value.slice(count);
-    }
-    const removeItem = (id) => {
-      messages.value = messages.value.filter(f => f.id !== id);
-    }
-    const checkOverflow = (_, done) => {
-      const chatMessages = chatMessage.value;
-      if (chatMessages) {
-        const badGuys = chatMessages.filter(f => {
-          return f.$el.getBoundingClientRect().top < 0
-        }).length;
-        removeMessages(badGuys);
-      }
+			messages.value = [onChatMessageEvent, ...messages.value];
+		};
+		const clearMessages = () => {
+			shouldClear.value = true;
+			const clearMSGTimeout = setTimeout(() => {
+				messages.value = [];
+				shouldClear.value = false;
+				clearTimeout(clearMSGTimeout);
+			}, 1200);
+		};
+		const removeMessages = (count) => {
+			messages.value = messages.value.slice(count);
+		};
+		const removeItem = (id) => {
+			messages.value = messages.value.filter((f) => f.id !== id);
+		};
+		const checkOverflow = (_, done) => {
+			const chatMessages = chatMessage.value;
+			if (chatMessages) {
+				const badGuys = chatMessages.filter((f) => {
+					return f.$el.getBoundingClientRect().top < 0;
+				}).length;
+				removeMessages(badGuys);
+			}
 			done();
-    }
+		};
 
 		onMounted(() => {
-			const webSocket = ref(new WebSocket(`ws://${window.location.host}/socket`));
+			const webSocket = ref(
+				new WebSocket(`ws://${window.location.host}/socket`)
+			);
 
-			webSocket.value.onmessage = function(e) {
+			webSocket.value.onmessage = function (e) {
 				const event = JSON.parse(e.data);
 				if (event.type === 'onChatMessage') {
 					addMessage(event.payload);
@@ -48,38 +50,37 @@ const chat = createApp({
 				} else if (event.type === 'onParty') {
 					isParty.value = true;
 					setTimeout(() => {
-						isParty.value = false;	
-					}, 20000);
+						isParty.value = false;
+					}, 45000);
 				}
-			}
+			};
 
-			webSocket.value.onopen = function(e) {
+			webSocket.value.onopen = function (e) {
 				console.log('Connection opened');
-			}
+			};
 
-			webSocket.value.onerror = function(e) {
+			webSocket.value.onerror = function (e) {
 				console.log('Error: ', e);
-			}
+			};
 
-			webSocket.value.onclose = function(e) {
+			webSocket.value.onclose = function (e) {
 				console.log('Connection closed');
 				window.location.reload();
-			}
+			};
 		});
 
-		return { messages, chatMessage, checkOverflow, removeItem, shouldClear}
+		return { messages, chatMessage, checkOverflow, removeItem, shouldClear };
 	},
 	computed: {
 		hasHighlight() {
-			return this.messages.some(m => m.flags.highlighted);
-		}
+			return this.messages.some((m) => m.flags.highlighted);
+		},
 	},
-	template:
-	`<div class="chat" :class="{ fade: hasHighlight }">
+	template: `<div class="chat" :class="{ fade: hasHighlight }">
 	<transition-group name="list" @enter="checkOverflow">
 		<chatMessage ref="chatMessage" v-for="(message, index) in messages" :key="message.id" :onChatMessageEvent="message" :ind="index" :total="messages.length" :getOuttaHere="shouldClear" v-on:removeItem="removeItem"></chatMessage>
 	</transition-group>
-</div>`
+</div>`,
 });
 
 chat.component('chatMessage', {
@@ -93,15 +94,17 @@ chat.component('chatMessage', {
 				hideMe.value = true;
 				setTimeout(() => {
 					emit('removeItem', onChatMessageEvent.id);
-				}, 1100)
+				}, 1100);
 			}, 60000);
-		})
+		});
 
 		onUnmounted(() => {
 			clearTimeout(destroyTimeout.value);
-		})
-		
-		const bgImage = computed(() => `url(${onChatMessageEvent.user.avatar_url})`);
+		});
+
+		const bgImage = computed(
+			() => `url(${onChatMessageEvent.user.avatar_url})`
+		);
 
 		return { hideMe, bgImage };
 	},
@@ -117,7 +120,7 @@ chat.component('chatMessage', {
 				return 'shake2';
 			}
 			return 'shake';
-		}
+		},
 	},
 	template: `
 	<div class="message" :class="{ shake, hide: getOuttaHere || hideMe, highlighted: onChatMessageEvent.flags.highlighted, mod: onChatMessageEvent.flags.mod, vip: onChatMessageEvent.flags.vip, gift: onChatMessageEvent.isRegistered }" >
@@ -135,8 +138,7 @@ chat.component('chatMessage', {
 			</div>
 		</div>
 	</div>`,
-  props: ['ind', 'onChatMessageEvent', 'total', 'getOuttaHere'],
+	props: ['ind', 'onChatMessageEvent', 'total', 'getOuttaHere'],
 });
 
 chat.mount('#app');
-
